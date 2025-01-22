3f90fd2e06d1cc2916405d7ad07cf39e6b423a51
Ange-Etoile <etoilematjabo@gmail.com>
2025-01-22 13:31:54 +0100

    creation du projet firebase effectuee

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("com.google.gms.google-services")
+
 }
 
 android {
@@ -36,7 +38,7 @@ android {
 }
 
 dependencies {
-
+    implementation(libs.firebase.bom)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
